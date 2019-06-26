8cfd31d8c3c0e48713e78d31b0e509d0147ec0e3
Rakesh.N <rkhnair94@gmail.com>
2019-06-26 11:20:07 +0530

    transfering Go_Web to new repo

--- GO_Web/Variable_File.sh
+++
@@ -1,5 +0,0 @@
-source /home/mint/DAILY_CHECK/rakesh_script/checkall/variables.sh
-
-for i in $stadd $cash;do echo "192.168.$i" ;done > hostfile1
-for i in $all_145 $future;do echo "192.168.$i" ;done > hostfile
-
